f17861c8251f03f9efb2bf9a806d03fdf7ff8087
Daniel Scarbrough <drscarbro@gmail.com>
2018-05-08 23:05:50 -0500

    Added Movie and Customer Model, View, and Controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        // GET: Movie
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Title = "Shrek!"},
+                new Movie {Id = 2, Title = "Wall-e"}
+            };
+
+        }
+    }
+}
