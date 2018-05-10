baa90815ef4df01801ae8d3b31ab87bafaafe786
Daniel Scarbrough <dscarbrough@arvest.com>
2018-05-10 08:12:46 -0500

    Added new customer action, view model, and model binding

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -23,11 +24,7 @@ namespace Vidly.Models
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Genre> Genres { get; set; }
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
+        public DbSet<MembershipType> MembershipTypes { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
